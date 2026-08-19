0b0c5063d581603b864f49ee016dffdaa237ab33
cardinaler <ruslan05kasumov@mail.ru>
2026-08-19 20:31:27 +0300

    preprocessing and saving done

---
+++ src/clean_and_save_dataset.py
@@ -0,0 +1,15 @@
+from preprocessor import SpotifyDataPreprocessor
+import pandas as pd
+import numpy as np
+import os
+
+path = os.getcwd()
+
+path_to_data = path + '/data/spotify_data/'
+path_to_save = path + '/data/processed/'
+preprocessor = SpotifyDataPreprocessor()
+
+df = pd.read_csv(path_to_data + 'dataset.csv')
+X, df_meta = preprocessor.fit_transform(df)
+np.save(path_to_save + 'features.npy', X)
+df_meta.to_csv(path_to_save + 'meta.csv')
---
+++ src/preprocessor.py
@@ -0,0 +1,83 @@
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import StandardScaler, QuantileTransformer, RobustScaler, FunctionTransformer
+import numpy as np
+import pandas as pd
+
+
+def sin_cos_transformer(x, period) -> np.ndarray:
+    x = np.asarray(x)
+    sin_key = np.sin(2 * np.pi * x / period)
+    cos_key = np.cos(2 * np.pi * x / period)
+    return np.column_stack([sin_key, cos_key])
+
+
+def get_sin_cos_names(transformer, input_features):
+    names = []
+    for col in input_features:
+        names.extend([f"{col}_sin", f"{col}_cos"])
+    return names
+
+
+def bin_time_signature(x) -> np.ndarray:
+    x = np.asarray(x)
+    x = np.where(x == 4, 1, 0)
+    return x.reshape(-1, 1)
+
+
+class SpotifyDataPreprocessor:
+    META_COLS = ['track_id', 'artists',
+                 'album_name', 'track_name', 'track_genre']
+
+    def __init__(self, random_state=42):
+        self.random_state = random_state
+
+        key_transformer = FunctionTransformer(
+            sin_cos_transformer,
+            kw_args={'period': 12},
+            feature_names_out=get_sin_cos_names
+        )
+
+        time_signature_transformer = FunctionTransformer(
+            bin_time_signature,
+            feature_names_out=lambda trans, in_feat: ['time_signature']
+        )
+
+        self.ct = ColumnTransformer(
+            transformers=[
+                ('drop_cols', 'drop', [
+                    'Unnamed: 0', 'track_id', 'artists', 'album_name', 'track_name', 'track_genre']),
+                ('std', StandardScaler(), ['popularity', 'energy', 'valence']),
+                ('quantile', QuantileTransformer(output_distribution='normal', random_state=self.random_state), [
+                 'speechiness', 'acousticness', 'instrumentalness', 'liveness']),
+                ('robust', RobustScaler(), [
+                 'duration_ms', 'danceability', 'loudness', 'tempo']),
+                ('key_cyclical', key_transformer, ['key']),
+                ('bin', time_signature_transformer, ['time_signature']),
+                ('bool_to_int', FunctionTransformer(np.int64), ['explicit'])
+
+            ],
+            remainder='passthrough'
+        )
+
+    def clean_data(self, df: pd.DataFrame):
+        df_clean = df.copy()
+        mask = (
+            (df_clean['duration_ms'] > 30 * 1000) &
+            (df_clean['duration_ms'] < 10 * 60 * 1000) &
+            (df_clean['loudness'] >= -25) &
+            (df_clean['tempo'] >= 1) &
+            (df_clean['time_signature'] != 0)
+        )
+        df_clean = df_clean[mask]
+
+        return df_clean
+
+    def fit_transform(self, df: pd.DataFrame):
+        df_clean = self.clean_data(df)
+        df_meta = df_clean[self.META_COLS]
+        X_scaled = self.ct.fit_transform(df_clean)
+
+        return X_scaled, df_meta
+
+    def get_feature_names_out(self):
+        return self.ct.get_feature_names_out()
